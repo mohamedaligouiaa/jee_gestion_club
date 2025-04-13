3a30bd8d626a4736a8e9ab53fdf75f7f172486d1
mohamedaligouiaa <mohamedaligouiaa12345@gmail.com>
2025-04-13 22:41:15 +0100

    Update Club.java

--- src/main/java/club/Club.java
+++ src/main/java/club/Club.java
@@ -11,10 +11,12 @@ import java.util.ArrayList;
 
 public class Club {
 	private int id;
+	private String image;
 	private String nom;
 	private String description;
-	public Club(int id, String nom, String description) {
+	public Club(int id,String image, String nom, String description) {
 		this.id = id;
+		this.image = image;
 		this.nom = nom;
 		this.description = description;
 	}
@@ -23,6 +25,12 @@ public class Club {
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
 	}
 	public String getNom() {
 		return nom;
@@ -36,7 +44,7 @@ public class Club {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public static ArrayList<Club> afficherClubs() {
+	public static ArrayList<Club> afficherClubs() throws SQLException {
         ArrayList<Club> liste = new ArrayList<>();
         Connection c = DatabaseConnection.getConnection(); 
 
@@ -44,13 +52,13 @@ public class Club {
             System.out.println("Erreur : Connexion à la base de données non établie !");
             return liste;
         }
-
+ 
         try {
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM club");
 
             while (rs.next()) {
-                Club cl = new Club(rs.getInt("idclub"), rs.getString("nom"), rs.getString("description"));
+                Club cl = new Club(rs.getInt("id"),rs.getString("image"), rs.getString("nom"), rs.getString("description"));
                 liste.add(cl);
             }
         } catch (SQLException e) {
